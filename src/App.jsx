9fa8ce48a5310d21b95c8a056c2e466c5529094a
unknown <omthokar900@gmail.com>
2023-08-02 18:28:12 +0545

    Project inti

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main-div'>
+    <div className='sub-div'><h1>Facebook</h1>
+    <img src="https://cdn.siasat.com/wp-content/uploads/2021/01/1200-900-Facebook.jpg"  style={{
+      height: 100,
+      width: 100,}} alt='Facebook'></img>
+      <br/>
+    <a href='www.facebook.com' >Visit Site</a>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Delectus ratione expedita asperiores. Aut consequatur facere nisi! 
+      Consectetur, dolores magni esse harum quidem sunt enim ipsum aperiam ducimus,
+       nam, alias et!</p>
+     </div>
+    <div className='sub-div'><h1>Instagram</h1></div>
+    <div className='sub-div'><h1>Tiktok</h1></div>
+    <div className='sub-div'><h1>X</h1></div>
+    </div>
+  )
+}
+
+export default App
